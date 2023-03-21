b0366dc2b09c2dd5609f6e7ad30a3e62e84524e8
AlexKimathi <keemalekiegmail.com>
2023-03-21 18:53:31 +0300

    adds prisma schema for model

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,1 +1,9 @@
-import prisma client from 
+
+import {PrismaClient}from '@prisma/client'
+
+const client = global.prismadb || new PrismaClient()
+
+if(process.env.NODE_ENV === 'production') global.prismadb = client
+
+
+export default client
